66b09979c5d48d384401680d2538d7946a45234a
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-02-09 17:11:37 +0530

    use routes and add that which type of data can express app is understand

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 import express from "express";
 import "./config/dbConfig.js";
+import cookieParser from "cookie-parser";
+import routes from "./routes/v1/index.js";
 
 const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/v1", routes);
+
 
 export default app;
 
