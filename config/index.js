e30ada20e3d7939742e9058bcc1bc401ef556146
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-02-09 17:03:06 +0530

    add jwt secrets and keys

--- config/index.js
+++ config/index.js
@@ -5,6 +5,12 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT,
     DB_URL: process.env.MONGODB_URL,
+    jwt: {
+        ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || "random-secret",
+        ACCESS_EXPIRATION_MINUTES: process.env.JWT_ACCESS_EXPIRATION_MINUTES || 30,
+        VERIFICATION_SECRET: process.env.JWT_VERIFICATION_SECRET || "random-secret",
+        VERIFICATION_EXPIRATION_MINUTES: process.env.JWT_VERIFICATION_EXPIRATION_MINUTES || 5,
+    },
 }
 
 export default config;
