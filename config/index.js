9a17f0969891e3de88e3b84e7920c2bcbf172f8b
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-02-12 20:55:45 +0530

    add cloudinary api key and secret

--- config/index.js
+++ config/index.js
@@ -11,6 +11,11 @@ const config = {
         VERIFICATION_SECRET: process.env.JWT_VERIFICATION_SECRET || "random-secret",
         VERIFICATION_EXPIRATION_MINUTES: process.env.JWT_VERIFICATION_EXPIRATION_MINUTES || 5,
     },
+    cloudinary: {
+        CLOUD_NAME: process.env.CLOUD_NAME,
+        CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+        CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    },
 }
 
 export default config;
