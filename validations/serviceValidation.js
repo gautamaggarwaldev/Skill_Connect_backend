3eac754c720d591e4c85df8b2428256b19bcb2ef
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-02-14 15:11:57 +0530

    validations for service creation and updation

---
+++ validations/serviceValidation.js
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+const createServiceValidation = Joi.object({
+    name: Joi.string().required().trim(),
+    description: Joi.string().required().trim(),
+    duration: Joi.number().integer().min(1).required(),
+    price: Joi.number().min(0).required(),
+    active: Joi.boolean().optional(),
+});
+
+const updateServiceValidation = Joi.object({
+    name: Joi.string().optional().trim(),
+    description: Joi.string().optional().trim(),
+    duration: Joi.number().integer().min(1).optional(),
+    price: Joi.number().min(0).optional(),
+    active: Joi.boolean().optional(),
+});
+
+export { createServiceValidation, updateServiceValidation };
