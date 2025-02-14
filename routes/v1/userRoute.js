1c57b65b6848ec0999712860cf9e337222a0d28d
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-02-14 11:56:12 +0530

    add more routes

--- routes/v1/userRoute.js
+++ routes/v1/userRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protect } from "../../middlewares/auth.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
-import { getUser } from "../../controllers/userController.js";
+import { getUser, updateUserProfileController, uploadPhoto } from "../../controllers/userController.js";
+import { upload } from "../../middlewares/upload.js";
+import { updateUserProfileValidation } from "../../validations/userValidation.js";
+import validate  from "../../middlewares/validate.js";
 
-const router = express.Router();
+const router = express.Router(); 
 
 router.get("/", protect, asyncHandler(getUser));
+router.post("/uploadphoto", protect, upload.single("avtar"), asyncHandler(uploadPhoto));
+router.put("/updateprofile", protect, validate(updateUserProfileValidation) ,asyncHandler(updateUserProfileController));
 
 export default router;
